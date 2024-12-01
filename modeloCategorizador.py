e36f8b44c3f527370b1cf8b67c84e0080ea6dd31
bueno-armando <armandoseb.bueno@gmail.com>
2024-12-01 09:08:28 -0600

    UPD: modeloCategorizador.py

--- modeloCategorizador.py
+++ modeloCategorizador.py
@@ -1,6 +1,12 @@
 import cv2
 from ultralytics import YOLO
 import subprocess
+import time
+
+# Definimos los requests por segundo deseados
+REQUESTS_PER_SECOND = 2  # Ajusta este valor según necesites
+INTERVAL = 1.0 / REQUESTS_PER_SECOND
+last_request_time = 0
 
 def execute_curl_request(signal):
     # La cadena original con "plastic"
@@ -9,6 +15,7 @@ def execute_curl_request(signal):
 -H "Content-Type: application/json" \
 -H "Content-Length: 33" \
 -d '{"data": {"grupo": "plastic"}}' '''
+
     
     # Reemplazar "plastic" por el valor de "signal"
     modified_command = curl_command.replace("plastic", signal)
@@ -17,7 +24,10 @@ def execute_curl_request(signal):
     subprocess.run(modified_command, shell=True, check=True)
 
 
-
+## Diccionario
+# metals_and_plastic', 'other', 'non_recyclable', 'glass', 'paper', 'bio', 'unknown'
+labelsDict = {"metals_and_plastic": "plastic", "other": "", "paper": "paper",
+              "bio": "bio", "unknown": "metal"}
 
 # Carga el modelo entrenado
 model = YOLO('modelo/modeloYOLO.pt')
@@ -55,20 +65,24 @@ while True:
         # Formatea los datos como cadenas de texto
         detection_info = f"Clase: {class_name}, Confianza: {confidence:.2f}, Coordenadas: ({x1:.0f}, {y1:.0f}, {x2:.0f}, {y2:.0f})"
 
-        # Imprime la información de la detección
-        print(f"********* {type(detection_info)} ***************")
-        print(detection_info)
+        # Imprime la información de la detección (DEBUG)
+        #print(f"********* {type(detection_info)} ***************")
+        #print(detection_info)
 
-        
-
-        # Ejemplo de uso
-        #signal_value = "bio"  # Puedes cambiar esto a cualquier valor que desees
-        #execute_curl_request(signal_value)
+        # Controlamos la frecuencia de los requests
+        current_time = time.time()
+        if current_time - last_request_time >= INTERVAL:
+            signal = labelsDict[class_name]
+            execute_curl_request(signal)
+            last_request_time = current_time
 
         # Opcional: Dibujar la caja y la etiqueta en el fotograma
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
         cv2.putText(frame, f"{class_name} {confidence:.2f}", (int(x1), int(y1) - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        
+        
+        
 
     # Muestra el fotograma con las detecciones
     cv2.imshow('Detecciones', frame)
